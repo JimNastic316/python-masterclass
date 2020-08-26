db1fd94dc1517f067f573c221fb4c6224cfbc17a
JimNastic316 <JimNastic316@gmail.com>
2020-08-26 12:23:07 -0400

    Commit

--- HelloWorld/escapechar.py
+++ HelloWorld/escapechar.py
@@ -4,6 +4,23 @@ print (splitString)
 tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 
-print('The pet shop owner said "No, no, \'e \'s uh,...he\'s resting".')
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
 #or
-print("The pet shop ownber said \"no, no, 'e's uh,...he's resting\".")
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+
+print(anotherSplitString)
+# use '\' to remove whitespace from output
+yetAnotherSplitString = """This string has not been \
+split over \
+several \
+lines"""
+
+print(yetAnotherSplitString)
+
