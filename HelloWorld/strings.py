0ce9d572533f172eabea45fc7f7ff77a2ffc84a2
JimNastic316 <JimNastic316@gmail.com>
2020-08-28 15:04:49 -0400

    Add
    Added operators.py

--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -1,14 +1,26 @@
-
+print("Today is a good day to learn Python")
 print('Python is fun')
 print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "Hello"
-name = input("Please enter your name ")
+name = "Jim"
 print(greeting + name)
 
 #if we want a space, we can add that too
 print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+#rebind value of variable "age"
+age_in_words = "Twenty Four"
+print(age)
+print(name + " is" + age + " years old") #won't work, can't concatonate strings with integers
+print(type(age))
+
 
 
 
