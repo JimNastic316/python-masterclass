1322c17a5480f0d0020ee0c0ff767cee310ddfaf
Jim <jimnastic316@gmail.com>
2020-09-13 18:56:17 -0400

    Updated strings2.py
    exercise

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -1,45 +1,11 @@
-#                   1
-#         01234567890123
-#
-parrot = "Norwegian Blue"
-print(parrot)
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+number ="9,223;372:036 854,775;807"
+separators = ""
 
-print()
-# using negative indexing
-print(parrot[-11])
-print(parrot[-10])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
-# or even better, just subtract 14 from original
-print()
+for char in number:
+    if not char.isnumeric():
+        separators = separators + char
 
-print(parrot[3 - 14])
-print(parrot[4 - 14])
-print(parrot[9 - 14])
-print(parrot[3 - 14])
-print(parrot[6 - 14])
-print(parrot[8 - 14])
+print(separators)
 
-#          01234567890123
-#parrot = "Norwegian Blue"
-
-#slicing
-print(parrot[:9])  # Norwegian
-print(parrot[10:]) # Blue
-
-# using a step in a slice
-print()
-letters = "abcdefghijklmnopqrstuvwxyz"
-print(letters[0:26:2])
-print(letters[1:26:2])
-print()
-print(letters[0:26:4])
-print(letters[1:26:4])
+values = "".join(char if char not in separators else " "  for char in number).split()
+print([int(val) for val in values])
