c89aa6a85d421f3dc59ae87f18a25ee956b072f8
JimNastic316 <JimNastic316@gmail.com>
2020-09-16 17:34:46 -0400

    Commit
    Adding

---
+++ FlowControl/aa.py
@@ -0,0 +1,31 @@
+x = 23
+print(x)
+
+x += 1 # 24
+print(x)
+
+x -= 4 # 20
+print(x)
+
+x *= 5 # 100
+print(x)
+
+x //= 4 # 25print(x)
+print(x)
+
+x /= 5  # 5.0 - converts from int to float
+print(x)
+
+x ** 2 # 25.0 - still represents a float
+print(x)
+
+x %= 5 # 0.0 - 25 is evenly diviisible by 5
+print(x)
+
+greeting = "Good "
+
+greeting += "morning! "
+print(greeting)
+
+greeting *= 5
+print(greeting)
