0c91cbc741a6f309c8c7ebf337edf59055df6a85
JimNastic316 <JimNastic316@gmail.com>
2020-09-30 16:18:12 -0400

    Adding outliers.py
    Exercise

--- Sequences/lists_intro.py
+++ Sequences/lists_intro.py
@@ -5,13 +5,8 @@ computer_parts = ["computer",
                   "mouse mat"
                   ]
 
-for part in computer_parts:
-    print(part)
+print(computer_parts)
 
-print()
-print(computer_parts[2])
-# Note how slicing a list generates another list in brackets
-print(computer_parts[0:3])
-
-# Prints the last entry in the list
-print(computer_parts[-1])
+#computer_parts[3] = "trackball"
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
