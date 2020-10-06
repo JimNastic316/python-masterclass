2b15f87d5ebb1e2cf509f950c11d32a84fcb8cce
JimNastic316 <JimNastic316@gmail.com>
2020-10-06 15:58:18 -0400

    updating
    exercising

--- Sequences/number_lists.py
+++ Sequences/number_lists.py
@@ -1,16 +1,12 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-# print(min(even))
-# print(max(even))
-# print(min(odd))
-# print(max(odd))
-# print()
-# print(len(even))
-# print(len(odd))
-#
-# print()
-# print("mississippi".count("s"))
-# print("mississippi".count("is"))
-# print("mississippi".count("issi"))
+numbers = [even, odd]
+print(numbers)
 
+for number_list in numbers:
+    print(number_list)
+
+    for value in number_list:
+        print(value)
