85404a3da8877c3635c8c30f1277b6139a9d5b7f
JimNastic316 <JimNastic316@gmail.com>
2020-10-21 13:15:56 -0400

    Tuple exercises

--- Sequences/tuples_intro.py
+++ Sequences/tuples_intro.py
@@ -6,10 +6,22 @@ budgie = ("Nightflight", "Budgie", 1981)
 imelda = ("More Mayhem", "Emilda May", 2011)
 metallica = ("Ride the Lightning", "Metallica", 1984)
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
 
-metallica2 = list(metallica)
-print(metallica2)
+#unpack tuple
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
+
+table = ("Coffee table", 200, 100, 75, 34.50)
+# Not so easy to understand that below code displays
+# length * width
+print(table[1] * table[2])
+
+# Better way is to unpack tuple
+name, length, width, height, price = table
+print(length * width)
