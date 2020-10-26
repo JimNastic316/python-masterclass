7b3722026be3619c04e1286655cfb864bf1bdf33
JimNastic316 <JimNastic316@gmail.com>
2020-10-26 16:05:12 -0400

    Nested indexing
    Nested indexing exercises and mini challenge

---
+++ Sequences/nested_indexing_minichallenge.py
@@ -0,0 +1,53 @@
+albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+         (1, "Welcome to my Nightmare"),
+         (2, "Devil's Food"),
+         (3, "The Black Widow"),
+         (4, "Some Folks"),
+         (5, "Only Women Bleed"),
+     ]
+     ),
+    ("Bad Company", "Bad Company", 1974,
+     [
+         (1, "Can't Get Enough"),
+         (2, "Rock Steady"),
+         (3, "Ready for Love"),
+         (4, "Don't Let Me Down"),
+         (5, "Bad Company"),
+         (6, "The Way I Choose"),
+         (7, "Movin' On"),
+         (8, "Seagull"),
+     ]
+     ),
+    ("Nightflight", "Budgie", 1981,
+     [
+         (1, "I Turned to Stone"),
+         (2, "Keeping a Rendezvous"),
+         (3, "Reaper of the Glory"),
+         (4, "She Used Me Up"),
+     ]
+     ),
+    ("More Mayhem", "Imelda May", 2011,
+     [
+         (1, "Pulling the Rug"),
+         (2, "Psycho"),
+         (3, "Mayhem"),
+         (4, "Kentish Town Waltz"),
+     ]
+     ),
+]
+# Challenge
+# Use nested indexng to print
+# "The way I choose" from Bad Company
+print(albums[1][3][5][1])
+#
+# The year Nightflight was released
+print(albums[2][2])
+#
+# Track number of song Kentish Town Waltz
+print(albums[3][3][3][0])
+#
+# The tuple representing the song Keeping the Rendezvous
+# from Budgie album Nightflight
+print(albums[2][3][1])
