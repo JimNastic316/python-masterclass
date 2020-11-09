88bc8cce578e9af665df37bc41eb813ff9112722
JimNastic316 <JimNastic316@gmail.com>
2020-11-09 15:32:51 -0500

    Banner.py
    Added banner exericise

---
+++ Functions_Intro/banner.py
@@ -0,0 +1,27 @@
+def banner_text(text):
+    screen_width = 50
+    if len(text) > screen_width - 4:
+        raise ValueError("String {0} is larger than specified width {1}"
+                         .format(text, screen_width))
+
+    if text =="*":
+        print("*" * screen_width)
+    else:
+        centerd_text = text.center(screen_width - 4)
+        output_string = "**{0}**".format(centerd_text)
+      # could have just done it all at once like below
+      # output_string = "**{0}**".format(text.center(screen_wdith - 4))
+        print(output_string)
+
+
+banner_text("*")
+banner_text("Always look on the bright side of life...")
+banner_text("If life seems jolly rotten,")
+banner_text("There's something you've forgotten!")
+banner_text("And that's to laugh and smile and dance and sing,")
+banner_text(" ")
+banner_text("When you're felling in the dumps,")
+banner_text("Don't be silly chumps,")
+banner_text("Just purse your lips and whistle - that's the thing!")
+banner_text("And... always look on the bright side of life...")
+banner_text("*")
