7de321c9102c6dc64b4a3147b04fe61966cec868
JimNastic316 <JimNastic316@gmail.com>
2020-11-30 14:28:20 -0500

    hilo_test
    Added hilo_test exercise

--- Functions_Intro/color_print.py
+++ Functions_Intro/color_print.py
@@ -1,3 +1,6 @@
+import colorama
+
+
 # Some ANSI escape sequences for colours and effects
 BLACK = '\u001b[30m'
 RED = '\u001b[31m'
@@ -12,3 +15,27 @@ RESET = '\u001b[0m'
 BOLD = '\u001b[1m'
 UNDERLINE = '\u001b[4m'
 REVERSE = '\u001b[7m'
+
+
+def color_print(text: str, effect: str) -> None:
+    """
+    Print `text` using ANSI sequences to change color, etc
+
+    :param text: The text to print.
+    :param effect: The effect we want. One of the constants
+        defined at the start of this module.
+    """
+    output_string = "{0}{1}{2}".format(effect, text, RESET)
+    print(output_string)
+
+colorama.init()
+color_print("Hello, Red", RED)
+# vfy that the color was reset
+print("This should be the default terminal color")
+color_print("Hello, Blue", BLUE)
+color_print("Hello, Yellow", YELLOW)
+color_print("Hello, Bold", BOLD)
+color_print("Hello, Underline", UNDERLINE)
+color_print("Hello, Reverse", REVERSE)
+color_print("Hello, Black", BLACK)
+colorama.deinit()
