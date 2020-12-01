efbfbe45d79f3ff6ecee0e5e4a51ed482c0847f3
JimNastic316 <JimNastic316@gmail.com>
2020-12-01 16:33:13 -0500

    fizz_buzz done
    Completed fizz buzz game

--- Functions_Intro/fizz_buzz.py
+++ Functions_Intro/fizz_buzz.py
@@ -1,6 +1,6 @@
-LOW = 1
-HIGH =100
-def fizz_buzz(num: int) ->str:
+LOW = int(1)
+HIGH = int(100)
+def fizz_buzz(num: object) -> object:
     """
     Function takes a variable and checks to see if it is divisible
     by 3, 5, or both.
@@ -27,9 +27,22 @@ def fizz_buzz(num: int) ->str:
         return ans
 
 
-for number in range(LOW, HIGH + 1):
-    print(fizz_buzz(number))
-
+# for next_number in range(LOW, HIGH + 1):
+#     print(fizz_buzz(next_number))
+next_number = 0
+print("Lets play fizz buzz, I'll go first")
+while next_number < 99:
+    next_number += 1
+    print("Computer : {}".format(fizz_buzz(next_number)))
+    next_number += 1
+    correct_answer = fizz_buzz(next_number)
+    players_guess = input("Player : ")
+    if players_guess != correct_answer:
+        print("Sorry, the correct answer is {}"
+              .format(correct_answer))
+        break
+else:
+    print("Good job, you did it!")
 
 
 
