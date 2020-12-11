b6d9af807b38615a1d133b51a47aeee9e1b87541
JimNastic316 <JimNastic316@gmail.com>
2020-12-11 14:29:59 -0500

    Updated Dictionary
    Started the updated dictionary project dictAndSet

--- Dictionaries/dictionaires.py
+++ Dictionaries/dictionaires.py
@@ -2,26 +2,12 @@ fruit = {"orange": "a sweet, orange, citus fruit",
          "apple": "good for making cider",
          "lemon": "a sour, yellow, citrus fruit",
          "grape": "a small sweet fruit growing in bunches",
-         "lime": "a sour, green citrus fruit",
-         "apple": "round and crunchy"}
+         "lime": "a sour, green citrus fruit"}
 
-# print(fruit)
-# print(fruit["lemon"])
-# fruit["pear"] = "an odd shaped apple"
-# print(fruit)
-# fruit["lime"] = "great with tequila"
-# print(fruit)
-# del fruit["lemon"]
-# print(fruit)
-#
-# print(fruit["tomato"])
+
 print(fruit)
-while True:
-    dict_key = input("Please enter a fruit:  ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("We dont' have a " + dict_key)
+
+for i in range(10):
+    for snack in fruit:
+        print(snack + " is " + fruit[snack])
+    print('-' * 40)
