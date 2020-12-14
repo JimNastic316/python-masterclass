2d870f71a8df72af6a7b75c98fe913ea1e77c8c7
JimNastic316 <JimNastic316@gmail.com>
2020-12-14 16:26:51 -0500

    DictionariesCont(350)
    Continued to work on the additional dictionary exercises

--- DictAndSet/dict_intro.py
+++ DictAndSet/dict_intro.py
@@ -1,23 +1,31 @@
 vehicles = {
     "dream": "Honda 250T",
-    "roadster": "BMW R1100",
     "er5": "Kawasaki ER5",
     "can-am": "Bomardier Can-Am 250",
     "virago": "Yamaha XV250",
     "tenere": "Yamaha XT650",
     "jimny": "Suziki Jimny 1.5",
     "fiesta": "Ford Fiesta Ghia 1.4",
+    "roadster": "Trimph Street Triple"
 }
 
-my_car = vehicles["fiesta"]
-print(my_car)
+vehicles["starfighter"] = "Lockhee F-104"
+vehicles["learjet"] = "Bombardier Lear jet 75"
+vehicles["toy"] = "glider"
 
-commuter = vehicles["virago"]
-print(commuter)
+#Upgrade the Virago
+vehicles["virago"] = "Yamaha XV535"
 
-learner = vehicles.get("er5")
-print(learner)
-#
-# for auto in vehicles:
-#     car = vehicles.get(auto)
-#     print(car)
+del vehicles ["starfighter"]
+# del vehicles["f1"]
+result = vehicles.pop("f1", "You wish. Sell the Learjet and maybe")
+print(result)
+plane = vehicles.pop("learjet")
+print(plane)
+
+bike = vehicles.pop("tenere", "not present")
+print(bike)
+print()
+
+for key, value in vehicles.items():
+    print(key, value, sep = ", ")
