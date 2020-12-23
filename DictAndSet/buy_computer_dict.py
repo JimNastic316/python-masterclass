d3273cd08f4693012233985b008fc013886c4195
JimNastic316 <JimNastic316@gmail.com>
2020-12-23 16:34:56 -0500

    Smart fridge exercise
    using dicionaries

--- DictAndSet/buy_computer_dict.py
+++ DictAndSet/buy_computer_dict.py
@@ -7,10 +7,24 @@ available_parts = {"1": "computer",
                    }
 
 current_choice = None
+
+# create an empty dictionary
+computer_parts = {}
+
 while current_choice != "0":
     if current_choice in available_parts:
         chosen_part = available_parts[current_choice]
-        print(f"Adding {chosen_part}")
+        # check to see if the user has chosen a computer part
+        # that is already in, remove it
+        if current_choice in computer_parts:
+            print(f"Removing {chosen_part}")
+            computer_parts.pop(current_choice)
+        else:
+            print(f"Adding {chosen_part}")
+            computer_parts[current_choice] = chosen_part
+
+        print(f"Your dictionary now contains: {computer_parts}")
+
     else:
         print("Please add options from the list")
         for key, value in available_parts.items():
