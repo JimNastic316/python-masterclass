6ce3ff09945f07c827e5b4f9e7dcae66fa773c0b
JimNastic316 <JimNastic316@gmail.com>
2021-01-06 14:00:36 -0500

    meal_planner.py update
    added sorting and printable list functionality

--- DictAndSet/meal_planner_mine.py
+++ DictAndSet/meal_planner_mine.py
@@ -1,20 +1,10 @@
 from contents import pantry, recipes
 
-
-def add_shopping_item(data: dict, item: str, amount: int) -> None:
-    """Add a tuple containing 'item' and 'amount' to the 'data' dictionary."""
-    if item in data:
-        data[item] += amount
-    else:
-        data[item] = amount
-
-
 display_dict = {}
-
 for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
 
-shopping_list = {} #empty dictionary
+
 while True:
     print("Please choose your recipe")
     print("-------------------------")
@@ -31,6 +21,7 @@ while True:
         print("checking ingredients ...")
         ingredients = recipes[selected_item]
         print(ingredients)
+        shopping_list = {} #emplty shopping list
         for food_item, required_quantity in ingredients.items():
             quanitiy_in_pantry = pantry.get(food_item, 0)
             if required_quantity <= quanitiy_in_pantry:
@@ -38,7 +29,6 @@ while True:
             else:
                 quanity_to_buy = required_quantity - quanitiy_in_pantry
                 print(f"\tYou need to buy {quanity_to_buy} of {food_item}")
-                add_shopping_item(shopping_list, food_item, quanity_to_buy)
-
-for things in shopping_list.items():
-    print(things)
+                shopping_list[food_item] = quanity_to_buy
+        print(f"You need to buy:  ")
+        print(shopping_list)
