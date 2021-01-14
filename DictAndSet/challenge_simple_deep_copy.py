399c3d824b796ce23370eb33e18ac3aec8f88f1b
JimNastic316 <JimNastic316@gmail.com>
2021-01-14 12:03:19 -0500

    challenge_simple_deepcopy
    deepcopy challenge

---
+++ DictAndSet/challenge_simple_deep_copy.py
@@ -0,0 +1,31 @@
+# write a function that takes a dictionary as an argument
+# and returns a deep copy of the dictionary without using
+# the 'copy' module
+
+from contents import recipes
+
+def my_deepcopy(dictionary: dict) -> dict:
+    """
+    Copy a dictionary, creating copies of the 'list' or 'dict' values
+    :param dictionary: The dictionary to copy
+    :return: a copy of dictionary, with values being copies of the original values
+    """
+    dictionary_copy = {}
+
+    for key, value in dictionary:
+        key_copy, value_copy = (key, value)
+
+
+# create a new, empty dictionary
+
+# iterate over the keys and values of the dictionary
+
+
+# for each key, copy the value,
+# then add a copy of the value to the dictionary
+
+# test data
+recipes_copy = my_deepcopy(recipes)
+recipes_copy["Butter chicken"]["ginger"] = 300
+print(recipes_copy["Butter chicken"]["ginger"])
+print(recipes["Butter chicken"]["ginger"])
---
+++ DictAndSet/deep_copy.py
@@ -0,0 +1,24 @@
+import copy
+
+lion_list = ["scary", "big", "cat"]
+elephant_list = ["big", "grey", "wrinkled"]
+teddy_list = ["cuddly", "stuffed"]
+
+animals = {
+    "lion": lion_list,
+    "elephant": elephant_list,
+    "teddy": teddy_list,
+}
+
+# Perorm a deep copy
+things = copy.deepcopy(animals)
+
+print(things["teddy"])
+print(animals["teddy"])
+
+print()
+
+things["teddy"].append("toy")
+print(things["teddy"])
+print(animals["teddy"])
+
