7f80e0e9c62e480b549c12fd05f57542bb138522
JimNastic316 <JimNastic316@gmail.com>
2021-02-02 10:04:45 -0500

    Sect 191 finished
    File IO finish

--- FileIO/fileio.py
+++ FileIO/fileio.py
@@ -10,5 +10,31 @@
 #         if "JAB" in line.upper():  # used upper in this example
 #             print(line, end = '')  # changes to single space, and newline
 # Using READLINE()
+# with open("sample.txt", 'r') as jabber:
+#     line = jabber.readline()
+#     while line:
+#         print(line, end = '')
+#         line = jabber.readline()
+
+# using READLINES()
 with open("sample.txt", 'r') as jabber:
-    line = jabber.readline()
+    lines = jabber.readlines()
+print(lines)
+
+for line in lines:
+    print(line, end ='')
+# Print list of lines in reverse, preserving the lines
+with open("sample.txt", 'r') as jabber:
+    lines = jabber.readlines()
+print(lines)
+
+for line in lines[::-1]:    # this reads in reverse
+    print(line, end ='')
+
+# Note the difference when READ() is used instead of READLINES()
+with open("sample.txt", 'r') as jabber:
+    lines = jabber.read()   # note this 
+print(lines)
+
+for line in lines[::-1]:
+    print(line, end ='')
