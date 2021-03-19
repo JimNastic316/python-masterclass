852e69284768979711ac5a9f254531b0f6dc5ab5
JimNastic316 <JimNastic316@gmail.com>
2021-03-19 15:02:51 -0400

    Sect 198 Updating shelve
    updating shelve exercise

--- FileIO/writing.py
+++ FileIO/writing.py
@@ -5,12 +5,29 @@
 #         print(city, file=city_file)
 
 # cities.txt already exists after preve is run
-cities = []
+# cities = []
+#
+# with open("cities.txt", 'r') as city_file:
+#     for city in city_file:
+#         cities.append(city.strip('\n'))  # strip takes of beginning or end, not in the middle
+#
+# print(cities)
+# for city in cities:
+#     print(city)
 
-with open("cities.txt", 'r') as city_file:
-    for city in city_file:
-        cities.append(city.strip('\n'))  # strip takes of beginning or end, not in the middle
+# imelda = "More Mayhem", "Imelda May", "2011", (
+#     (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish TOwn Waltz"))
+#
+# with open("imelda3.txt", 'w') as imelda_file:
+#     print(imelda, file=imelda_file)
 
-print(cities)
-for city in cities:
-    print(city)
+with open("imelda3.txt", 'r') as imelda_file:
+    contents = imelda_file.readline()
+# using eval is not a good idea when using files outside of your program
+imelda = eval(contents)
+
+print(imelda)
+title, artist, year, tracks = imelda
+print(title)
+print(artist)
+print(year)
