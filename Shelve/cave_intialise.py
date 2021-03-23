47b9b2b72fd234e8b7ae1f117cd6a4b6a9f2b45e
JimNastic316 <JimNastic316@gmail.com>
2021-03-23 15:05:32 -0400

    Finished File I/O
    Wrapped up section on I/O

--- Shelve/cave_intialise.py
+++ Shelve/cave_intialise.py
@@ -1,62 +1,42 @@
+# March 23 2021
+# Code along solution to Sect 199
+# https://www.udemy.com/share/101WaiB0Udc19bQ34=/
 import shelve
 
 locations = shelve.open('locations')
 
-locations = ['0'] = {"desc": "You are sitting in front of a computer learning Python",
-                 "exits": {},
-                 "namedExits": {}}
-locations = ['1'] = {"desc": "You are standing at the end of a road before a small brick building",
-                 "exits": {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-                 "namedExits": {"2": 2, "3": 3, "5": 5, "4": 4}}
-locations = ['2'] = {"desc": "You are at the top of a hill",
-                 "exits": {"N": 5, "Q": 0},
-                 "namedExits": {"5": 5}}
-locations = ['3'] = {"desc": "You are inside a building, a well house for a small stream",
-                 "exits": {"W": 1, "Q": 0},
-                 "namedExits": {"1": 1}}
-locations = ['4'] = {"desc": "You are in a valley beside a stream",
-                 "exits": {"N": 1, "W": 2, "Q": 0},
-                 "namedExits": {"1": 1, "2": 2}}
-locations = ['5'] = {"desc": "You are in the forest",
-                 "exits": {"W": 2, "S": 1, "Q": 0},
-                 "namedExits": {"2": 2, "1": 1}}
-             }
+locations['0'] = {"desc": "You are sitting in front of a computer learning Python",
+                  "exits": {},
+                  "namedExits": {}}
+locations['1'] = {"desc": "You are standing at the end of a road before a small brick building",
+                  "exits": {"W": '2', "E": '3', "N": '5', "S": '4', "Q": '0'},
+                  "namedExits": {"2": '2', "3": '3', "5": '5', "4": '4'}}
+locations['2'] = {"desc": "You are at the top of a hill",
+                  "exits": {"N": '5', "Q": '0'},
+                  "namedExits": {"5": '5'}}
+locations['3'] = {"desc": "You are inside a building, a well house for a small stream",
+                  "exits": {"W": '1', "Q": '0'},
+                  "namedExits": {"1": '1'}}
+locations['4'] = {"desc": "You are in a valley beside a stream",
+                  "exits": {"N": '1', "W": '2', "Q": '0'},
+                  "namedExits": {"1": '1', "2": '2'}}
+locations['5'] = {"desc": "You are in the forest",
+                  "exits": {"W": '2', "S": '1', "Q": '0'},
+                  "namedExits": {"2": '2', "1": '1'}}
 
-vocabulary = {"QUIT": "Q",
-              "NORTH": "N",
-              "SOUTH": "S",
-              "EAST": "E",
-              "WEST": "W",
-              "ROAD": "1",
-              "HILL": "2",
-              "BUILDING": "3",
-              "VALLEY": "4",
-              "FOREST": "5"}
+locations.close()
 
-loc = 1
-while True:
-    availableExits = ", ".join(locations[loc]["exits"].keys())
+vocabulary = shelve.open('vocabulary')
 
-    print(locations[loc]["desc"])
+vocabulary["QUIT"] = "Q"
+vocabulary["NORTH"] = "N"
+vocabulary["SOUTH"] = "S"
+vocabulary["EAST"] = "E"
+vocabulary["WEST"] = "W"
+vocabulary["ROAD"] = "1"
+vocabulary["HILL"] = "2"
+vocabulary["BUILDING"] = "3"
+vocabulary["VALLEY"] = "4"
+vocabulary["FOREST"] = "5"
 
-    if loc == 0:
-        break
-    else:
-        allExits = locations[loc]["exits"].copy()
-        allExits.update(locations[loc]["namedExits"])
-
-    direction = input("Available exits are " + availableExits  + ": " ).upper()
-    print()
-
-    # Parse the user input, using our vocabulary dictionary if necessary
-    if len(direction) > 1:  # more than 1 letter, so check vocab
-        words = direction.split()
-        for word in words:
-            if word in vocabulary:   # does it contain a word we know?
-                direction = vocabulary[word]
-                break
-
-    if direction in allExits:
-        loc = allExits[direction]
-    else:
-        print("You cannot go in that direction")
+vocabulary.close()
