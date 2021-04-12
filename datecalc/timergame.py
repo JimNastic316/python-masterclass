1029013b1402abafa828896ca9e728d7b97eb3b5
JimNastic316 <JimNastic316@gmail.com>
2021-04-12 15:42:18 -0400

    Sect 212 - Using timezones
    timezone exercises, just s

--- datecalc/timergame.py
+++ datecalc/timergame.py
@@ -1,6 +1,6 @@
 import time, random
 
-from time import time as my_timer
+from time import process_time as my_timer
 input("Press enter to start")
 wait_time = random.randint(1, 6)
 print(wait_time)
@@ -12,4 +12,5 @@ end_time = my_timer()
 
 print("Started at " + time.strftime("%X",time.localtime((start_time))))
 print("Ended at " + time.strftime("%X", time.localtime((end_time))))
+
 print(f"Your reaction time was {end_time - start_time} seconds.")
