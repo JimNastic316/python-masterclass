d400cc58ad47da423a5c30526adff4c089975656
JimNastic316 <JimNastic316@gmail.com>
2021-04-22 15:30:30 -0400

    Sect 216 - Tkinter pack geometry manager
    started tkinter, buttons, canvas

--- tkinter/tkinterdemo.py
+++ tkinter/tkinterdemo.py
@@ -1,11 +1,28 @@
 import tkinter
 
-print(tkinter.TkVersion)
-print(tkinter.TclVersion)
-
 mainWindow = tkinter.Tk()
 
 mainWindow.title('Hello World')
-mainWindow.geometry('640x400')
+mainWindow.geometry('640x400+8+400')
+
+label = tkinter.Label(mainWindow, text="Hello World")
+label.pack(side='top')
+
+label = tkinter.Label(main)
+
+canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
+
+canvas.pack(side='top')
+button1 = tkinter.Button(mainWindow, text="button1")
+button2 = tkinter.Button(mainWindow, text="button2")
+button3 = tkinter.Button(mainWindow, text="button3")
+
+button1.pack(side='top', anchor='n')
+button2.pack(side='top', anchor='s')
+button3.pack(side='top', anchor='e')
+
+
+
+
 mainWindow.mainloop()
 
