4438e0c90be90ac330b65c71488f43e8da9e7eb8
JimNastic316 <JimNastic316@gmail.com>
2021-04-22 09:50:35 -0400

    Sect 215 - Intro to tkinter
    started using tkinter GUI

---
+++ tkinter/tkinterdemo.py
@@ -0,0 +1,11 @@
+import tkinter
+
+print(tkinter.TkVersion)
+print(tkinter.TclVersion)
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title('Hello World')
+mainWindow.geometry('640x400')
+mainWindow.mainloop()
+
