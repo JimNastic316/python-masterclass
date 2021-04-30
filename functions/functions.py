fe2281625389d5e859575e8350084f6221b23154
JimNastic316 <JimNastic316@gmail.com>
2021-04-30 16:32:36 -0400

    Sect 224 Functions Part 3
    Finished up section on functions

--- functions/functions.py
+++ functions/functions.py
@@ -4,24 +4,24 @@ def python_food():
     left_margin = (width - len(text)) // 2
     print(" "  * left_margin, text)
 
-def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+
+def center_text(*args, sep=' '):
     text = ""
-    # text = str(text)
     for arg in args:
         text += str(arg) + sep
     left_margin = (30 - len(text) // 2)
-    print(" " * left_margin, text, end=end, file=file, flush=flush)
+    return " " * left_margin + text
 
+# with open("centered", mode='w') as centered_file:
 # call the function
-# python_food()
-center_text("spam and eggs")
-center_text("spam, spam and eggs")
-center_text("spam, spam, spam, and spam")
-# numbers have no len, but
-# "text = str(text)" converts text to string
-center_text(12)
+with open ("menu", "w") as menu:
+    s1 = center_text("spam and eggs")
+    print(s1, file=menu)
+    s2 = center_text("spam, spam and eggs")
+    print(s2, file=menu)
+    print(center_text(12), file=menu)
+    print(center_text("spam, spam, spam, and spam"), file=menu)
+    s5 = center_text("first", "second", 3, 4, "spam", sep=":")
+    print(s5, file=menu)
 
-center_text("first", "second", 3, 4, "spam", sep=":")
 
-# every python function returns a value, default = None
-# print(python_food())
