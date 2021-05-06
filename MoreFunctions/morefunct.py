be17efbecbdea9a88d9ad1a00acd9d52b2e47911
JimNastic316 <JimNastic316@gmail.com>
2021-05-06 14:43:22 -0400

    Sect 228 - Circle Challenge
    Section 228, circle challenge comp

--- MoreFunctions/morefunct.py
+++ MoreFunctions/morefunct.py
@@ -8,13 +8,16 @@ def parabola(page, size):
         plot(page, x, y)
         plot(page, -x, y)
 
-def circle(page, radius, g, h):
-    for x in range(g, g + radius):
-        y = h + (math.sqrt(radius ** 2 - ((x-g) ** 2)))
-        plot(page, x, y)
-        plot(page, x, 2 * h - y)
-        plot(page, 2 * g - x, y)
-        plot(page, 2 * g - x, 2 * h - y)
+def circle(page, radius, g, h, color="red"):
+    page.create_oval(g + radius, h + radius, g - radius, h - radius, outline = color, width=2)
+# Hard, slow way
+    # for x in range(g * 100, (g + radius) * 100):
+    #     x/=100
+    #     y = h + (math.sqrt(radius ** 2 - ((x-g) ** 2)))
+    #     plot(page, x, y)
+    #     plot(page, x, 2 * h - y)
+    #     plot(page, 2 * g - x, y)
+    #     plot(page, 2 * g - x, 2 * h - y)
 
 def draw_axes(page):
     page.update()
@@ -42,10 +45,10 @@ canvas.grid(row=0, column=0)
 draw_axes(canvas)
 parabola(canvas, 100)
 parabola(canvas, 200)
-circle(canvas, 100, 100, 100)
-circle(canvas, 100, 100, -100)
-circle(canvas, 100, -100, 100)
-circle(canvas, 100, -100, -100)
+circle(canvas, 100, 100, 100, 'green')
+circle(canvas, 100, 100, -100, 'yellow')
+circle(canvas, 100, -100, 100, 'black')
+circle(canvas, 100, -100, -100, 'blue')
 circle(canvas, 10, 30, 30)
 circle(canvas, 10, 30, -30)
 circle(canvas, 10, -30, 30)
