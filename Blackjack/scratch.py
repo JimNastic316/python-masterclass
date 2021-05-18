84ab7aea293886fcf3d16f7deadcd1e4418a9ea2
JimNastic316 <JimNastic316@gmail.com>
2021-05-18 15:32:49 -0400

    Sect 232 - Compare
    need to find differences

--- Blackjack/scratch.py
+++ Blackjack/scratch.py
@@ -1,8 +1,11 @@
 import random
-import tkinter
+
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
 
 
-# Load images
 def load_images(card_images):
     suits = ['heart', 'club', 'diamond', 'spade']
     face_cards = ['jack', 'queen', 'king']
@@ -11,26 +14,28 @@ def load_images(card_images):
         extension = 'png'
     else:
         extension = 'ppm'
-    #     # for each suit, retrieve the image for the cards
+
+    # for each suit, retrieve the image for the cards
     for suit in suits:
-        # first the number cards from 1 to 10 (quits at 11)
+    # first the number cards 1 to 10
         for card in range(1, 11):
             name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
             image = tkinter.PhotoImage(file=name)
             card_images.append((card, image,))
-        #         # next the face cards
-        for card in face_cards:
-            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
-            image = tkinter.PhotoImage(file=name)
-            card_images.append((10, image,))
+
+    # next the face cards
+    for card in face_cards:
+        name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+        image = tkinter.PhotoImage(file=name)
+        card_images.append((10, image,))
 
 
 def deal_card(frame):
-    # pop next card off deck
-    next_card = deck.pop(0)  # Use '0' to pop from top of deck, if blank would be from the bottom
-    # add the image to a label and display the label
+    # pop the next card off the top of the deck
+    next_card = deck.pop(0)
+    # add the image to a Label and display the label
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
-    # return the cards face value
+    # now return the card's face value
     return next_card
 
 
@@ -44,52 +49,52 @@ def deal_player():
     if card_value == 1 and not player_ace:
         card_value = 11
     player_score += card_value
-    print(player_score)
     # if we would bust, check if there is an ace and subtract
     if player_score > 21 and player_ace:
         player_score -= 10
-        print(player_score)
     player_score_label.set(player_score)
     if player_score > 21:
-        result_text.set("Dealer Wins!")
+        result_text.set("Dealer wins!")
+
 
 mainWindow = tkinter.Tk()
 
-# Set up screen and frames for the dealer and player
+# Set up the screen and frames for the dealer and player
 mainWindow.title("Black Jack")
-mainWindow.geometry('640x480')
+mainWindow.geometry("640x480")
 mainWindow.configure(background='green')
+
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
 result.grid(row=0, column=0, columnspan=3)
 
-card_frame = tkinter.Frame(mainWindow, relief='sunken', borderwidth=1, background='green')
+card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="green")
 card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
-tkinter.Label(card_frame, text='Dealer', background='green', fg='white').grid(row=0, column=0)
-tkinter.Label(card_frame, textvariable=dealer_score_label, background='green', fg='white').grid(row=1, column=0)
-# Embedded frame hold the card images
-dealer_card_frame = tkinter.Frame(card_frame, background='green')
-dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
+tkinter.Label(card_frame, text="Dealer", background="green", fg='white').grid(row=0, column=0)
+tkinter.Label(card_frame, textvariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
+# embedded frame to hold the card images
+dealer_card_frame = tkinter.Frame(card_frame, background="green")
+dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
 player_score = 0
 player_ace = False
 
-tkinter.Label(card_frame, text='Player', background='green', fg='white').grid(row=2, column=0)
-tkinter.Label(card_frame, textvariable=player_score_label, background='green', fg='white').grid(row=3, column=0)
+tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
 # embedded frame to hold the card images
-player_card_frame = tkinter.Frame(card_frame, background='green')
+player_card_frame = tkinter.Frame(card_frame, background="green")
 player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
 
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
-dealer_button = tkinter.Button(button_frame, text='Dealer', command=deal_dealer)
+dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text='Player', command=deal_player)
+player_button = tkinter.Button(button_frame, text="Player", command=deal_player)
 player_button.grid(row=0, column=1)
 
 # load cards
@@ -98,6 +103,7 @@ load_images(cards)
 print(cards)
 # Create a new deck of cards and shuffle them
 deck = list(cards)
+
 random.shuffle(deck)
 
 # Create the list to store the dealer's and player's hands
