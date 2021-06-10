93632ac689ffaab42ed03d5028360ee3f6e9534f
JimNastic316 <JimNastic316@gmail.com>
2021-06-10 14:43:44 -0400

    Sect 236 Comp

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -85,6 +85,12 @@ def deal_player():
     if player_score > 21:
         result_text.set("Dealer Wins!")
 
+def initial_deal():
+    deal_player()
+    dealer_hand.append(deal_card(dealer_card_frame))
+    dealer_score_label.set(score_hand(dealer_hand))
+    deal_player()
+
 
 def new_game():
     global dealer_card_frame
@@ -104,11 +110,7 @@ def new_game():
     # Create the list to store the dealer's and player's hands
     dealer_hand = []
     player_hand = []
-
-    deal_player()
-    dealer_hand.append(deal_card(dealer_card_frame))
-    dealer_score_label.set(score_hand(dealer_hand))
-    deal_player()
+    initial_deal()
 
 
 def shuffle():
@@ -116,11 +118,7 @@ def shuffle():
 
 
 def play():
-    deal_player()
-    dealer_hand.append(deal_card(dealer_card_frame))
-    dealer_score_label.set(score_hand(dealer_hand))
-    deal_player()
-
+    initial_deal()
     mainWindow.mainloop()
 
 
