b964db97ac34703f0e89db6f614a44ff6b1e1a39
JimNastic316 <JimNastic316@gmail.com>
2021-06-23 15:39:42 -0400

    Sect 238 Finished
    recursion exercise

--- ScopeAndNamespace/scope.py
+++ ScopeAndNamespace/scope.py
@@ -1,14 +1,12 @@
-# def fact(n):
-#     """ calculate n! iteratively """
-#     result = 1
-#     if n > 1:
-#         for f in range(2, n + 1):
-#             result *= f
-#     return result
-#
-# for i in range(130):
-#     print(i, fact(i))
-#
+def fact(n):
+    """ calculate n! iteratively """
+    result = 1
+    if n > 1:
+        for f in range(2, n + 1):
+            result *= f
+    return result
+
+
 def factorial(n):
     # n! could also be defined as n * (n-1)!
     """ calculates n! recursively """
@@ -17,5 +15,42 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
-for i in range(130):
-    print(i, factorial(i))
+
+def fib(n):
+    """ F(n) = F(n-1) + F(n-2)"""
+    if n < 2:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def fibonacci(n):
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        n_minus1 = 1
+        n_minus2 = 0
+        for f in range(1, n):
+            result = n_minus2 + n_minus1
+            n_minus2 = n_minus1
+            n_minus1 = result
+        return result
+
+# for i in range(130):
+#     print(i, fact(i))
+
+# for i in range(130):
+#     print(i, factorial(i))
+
+# Note how much slower the first function call is
+# Recursion is slower
+for i in range(36):
+    print(i, fib(i))
+
+for i in range(36):
+    print(i, fibonacci(i))
+
+# for i in range(36):
+#     print(i, fib(i), "\t", fibonacci(i))
