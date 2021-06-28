b5010fff115a4538281d35700208b644b014a256
JimNastic316 <JimNastic316@gmail.com>
2021-06-28 15:23:43 -0400

    Sect 242 - start
    about halfway through sect 242

--- oop/oop.py
+++ oop/oop.py
@@ -4,7 +4,10 @@ class Kettle(object):
         self.make = make
         self.price = price
         self.on = False
-        
+
+    def switch_on(self):
+        self.on = True
+
 
 kenwood = Kettle("Kenwood", 8.99)
 print(kenwood.make)
@@ -14,3 +17,7 @@ kenwood.price = 12.75
 print(kenwood.price)
 
 hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+print(f"Models: {kenwood.make} = {kenwood.price}, {hamilton.make} = {hamilton.price}")
