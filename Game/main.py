303429736dab44ce09834f95538a7a2a29ea70f3
JimNastic316 <JimNastic316@gmail.com>
2021-07-21 15:32:20 -0400

    Sect 257 Data attributes and properties
    finished

--- Game/main.py
+++ Game/main.py
@@ -4,12 +4,24 @@ tim = Player("Tim")
 
 print(tim.name)
 print(tim)
-tim.lives += -1
+tim.lives -= 1
 print(tim)
 
-tim.lives += -1
+tim.lives -= 1
 print(tim)
 
-tim.lives += -1
+tim.lives -= 1
+print(tim)
+
+tim._lives = 9
+print(tim)
+
+tim.level = 2
+print(tim)
+
+tim.level += 5
+print(tim)
+
+tim.level = 3
 print(tim)
 
