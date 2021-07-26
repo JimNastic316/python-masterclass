69a377602cd79b971572555c64d4da6d7df2e6a2
JimNastic316 <JimNastic316@gmail.com>
2021-07-26 15:32:40 -0400

    Sect 260 Subclasses and overloading
    class Enemy, subclass Troll

--- Game/main.py
+++ Game/main.py
@@ -2,7 +2,7 @@ from player import Player
 
 tim = Player("Tim")
 
-from enemy import Enemy
+from enemy import Enemy, Troll
 
 random_monster = Enemy("Basic enemy", 12, 1)
 print(random_monster)
@@ -15,3 +15,12 @@ print(random_monster)
 
 random_monster.take_damage(9)
 print(random_monster)
+
+ugly_troll = Troll()
+print("Ugly troll - {}".format(ugly_troll))
+
+another_troll = Troll("Ug", 18, 1)
+print("Another troll - {}".format(another_troll))
+
+brother = Troll("Urg", 23)
+print("Another troll - {}".format(brother))
