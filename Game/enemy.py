ea13cd62ec262927130b2a6f01cf727d9f802f99
JimNastic316 <JimNastic316@gmail.com>
2021-07-30 15:24:38 -0400

    Sect 311 Changing Behavior of methods
    Vampyre, Troll, and Enemy classes

--- Game/enemy.py
+++ Game/enemy.py
@@ -4,14 +4,20 @@ class Enemy:
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
         if remaining_points >= 0:
             self.hit_points = remaining_points
-            print('I took {} [points damage and have {} left'.format(damage, self.hit_points))
+            print('I took {} points damage and have {} left'.format(damage, self.hit_points))
         else:
             self.lives -= 1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+            else:  # Zero live left
+                print("{0.name} is dead".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
@@ -27,3 +33,12 @@ class Troll(Enemy):
 
     def grunt(self):
         print("Me {0.name}. {0.name} stomp you".format(self))
+
+
+class Vampyre(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
+
+    def talk(self):
+        print("I am Count {0.name}! Blah blah blah!".format(self))
--- Game/main.py
+++ Game/main.py
@@ -1,4 +1,4 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampyre
 
 ugly_troll = Troll("Pug")
 print("Ugly troll - {}".format(ugly_troll))
@@ -12,3 +12,20 @@ print("Another troll - {}".format(brother))
 ugly_troll.grunt()
 another_troll.grunt()
 brother.grunt()
+
+vampyre = Vampyre("Chocula")
+print("Vampyer - {}".format(vampyre))
+
+vampyre_2 = Vampyre("Chocula the 2nd")
+print("Another vampyre - {}".format(vampyre_2))
+
+vampyre.talk()
+vampyre_2.talk()
+
+vampyre.take_damage(3)
+vampyre_2.take_damage(4)
+another_troll.take_damage(3)
+
+while vampyre.alive:
+    vampyre.take_damage(1)
+    print(vampyre)
