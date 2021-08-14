f9b4a6210d1bd876699998aae1d8fccc006f1bf1
JimNastic316 <JimNastic316@gmail.com>
2021-08-14 17:27:16 -0400

    Sect 317 & 18 - Composition
    Composition is used to make a program that creates an HTML document

--- Game/html_doc.py
+++ Game/html_doc.py
@@ -1,11 +1,70 @@
-#8:00 sect 317
-class Tag():
+# Stopped at 2:10 on sect 319
+class Tag(object):
 
     def __init__(self, name, contents):
-        self-start_tag = '<{}'.format(name)
+        self.start_tag = '<{}>'.format(name)
         self.end_tag = '</{}>'.format(name)
-        se;f.contenst = contents
+        self.contents = contents
 
     def __str__(self):
         return "{0.start_tag}{0.contents}{0.end_tag}".format(self)
 
+    def display(self, file=None):
+        print(self, file=file)
+
+
+class DocType(Tag):
+    def __init__(self):
+        super().__init__('!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd', '')
+        self.end_tag = ''  # DOCTYPE doesn't have an end tag
+
+
+class Head(Tag):
+    def __init__(self):
+        super().__init__('head', '')
+
+
+class Body(Tag):
+    def __init__(self):
+        super().__init__('body', '')     # Body contents will be built up separately
+        self._body_contents = []
+
+    def add_tag(self, name, contents):
+        new_tag = Tag(name, contents)
+        self._body_contents.append(new_tag)
+
+    def display(self, file=None):
+        for tag in self._body_contents:
+            self.contents += str(tag)
+
+        super().display(file=file)
+
+
+class HtmlDoc(object):
+    # Class is composed of the classes above, has no data attributes of its own (doesn't mean has to be the case though)
+    def __init__(self):
+        self._doc_type = DocType()
+        self._head = Head()
+        self._body = Body()
+
+    def add_tag(self, name, contents):
+        # Method to add tags to a document
+        self._body.add_tag(name, contents)
+
+    def display(self, file=None):
+        self._doc_type.display(file=file)
+        print('<html>')
+        self._head.display(file=file)
+        self._body.display(file=file)
+        print('</html>', file=file)
+
+
+###########################################
+if  __name__ == '__main__':
+    my_page = HtmlDoc()
+    my_page.add_tag('h1', 'Main heading')
+    my_page.add_tag('h2', 'sub-heading')
+    my_page.add_tag('p', 'This is a paragragh that will appear on the page')
+    with open('test.html', 'w') as test_doc:
+        my_page.display(file=test_doc)  # Creates a file called test.html
+    my_page.display()
