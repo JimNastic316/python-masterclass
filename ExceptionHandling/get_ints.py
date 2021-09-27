25aba915571555ec6a92d6900debd98ccad8c675
JimNastic316 <JimNastic316@gmail.com>
2021-09-27 15:24:03 -0400

    Exception Challenge
    using try , except, built in exception codes

--- ExceptionHandling/get_ints.py
+++ ExceptionHandling/get_ints.py
@@ -1,8 +1,24 @@
-def getint():
+import sys
+
+def getint(prompt):
     while True:
         try:
-            num = int(input("Please enter a number "))
+            num = int(input(prompt))
             return num
         except ValueError:
             print("Invalid number entered, please try again")
+        except EOFError:
+            sys.exit(1)
 
+def divide(num1, num2):
+    try:
+        print("{} diveded by {} is {}".format(num1, num2, num1 / num2))
+    except ZeroDivisionError:
+        print("You can't divide my zero")
+        sys.exit(2)
+
+
+
+first_number = getint("Please enter the first number ")
+second_number = getint("Please enter the second number ")
+divide(first_number, second_number)
