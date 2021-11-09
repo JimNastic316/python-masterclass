413015f8bb5c615cafced9a9a56977d1ebf545e4
JimNastic316 <JimNastic316@gmail.com>
2021-11-09 15:34:50 -0500

    Kwargs
    kwargs

--- MusicBrowser/star_args.py
+++ MusicBrowser/star_args.py
@@ -1,10 +1,32 @@
-def average(*args):
-    print(type(args))
-    print("args is {}:".format(args))
-    print("args is:", *args)
-    mean = 0
-    for arg in args:
-        mean += arg
-    return mean / len(args)
+# def print_backwards(*args, end=' ', **kwargs):
+# def print_backwards(*args, **kwargs):
+#     print(kwargs)
+#     kwargs.pop('end', None)
+#     for word in args[::-1]:
+#         print(word[::-1], end=' ', **kwargs)
 
-print(average(1, 2, 3, 4))
+def print_backwards(*args, **kwargs):
+    end_character = kwargs.pop('end', '\n')
+    sep_character = kwargs.pop('sep', ' ')
+    for word in args[::-1]:
+        print(word[::-1], end=sep_character, **kwargs)
+    print(end=end_character)
+
+with open("backwards.txt", 'w') as backwards:
+    print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='\n')
+    print("Another string")
+
+print()
+print("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='\n', sep='|')
+print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='\n', sep='|')
+
+# def average(*args):
+#     print(type(args))
+#     print("args is {}:".format(args))
+#     print("args is:", *args)
+#     mean = 0
+#     for arg in args:
+#         mean += arg
+#     return mean / len(args)
+#
+# print(average(1, 2, 3, 4))
