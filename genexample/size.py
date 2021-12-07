e076199bd3d67818f19c4fc60c406934a10bc024
JimNastic316 <JimNastic316@gmail.com>
2021-12-07 15:30:24 -0500

    Sect 367 Generators and Yield
    just started with yield

---
+++ genexample/size.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def my_range(n: int):
+    start = 0
+    while start < n:
+        yield start
+        start += 1
+
+# big_range = range(10000)
+big_range = my_range(10000)
+
+print("big_range is {} bytes".format(sys.getsizeof(big_range)))
+
+# create a list containing al the values in big_range
+big_list = []
+for val in big_range:
+    big_list.append(val)
+
+print("big_list is {} bytes".format(sys.getsizeof(big_list)))
